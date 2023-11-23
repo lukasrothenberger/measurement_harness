1dcca4b2c3c5721df63028daf4f0481fc0a0c498
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-23 13:48:24 +0100

    updated harness

--- harness.sh
+++ harness.sh
@@ -1,8 +1,9 @@
-HOME_DIR=$(pwd)
+ HOME_DIR=$(pwd)
 ### SETTINGS ###
 DISCOPOP_PATH=/home/lukas/git/discopop
 DISCOPOP_BUILD=$DISCOPOP_PATH/build
-PROJECT_PATH=/home/lukas/git/Benchmarks/untested/miniFE/ref/src
+PROJECT_PATH=/home/lukas/Schreibtisch/Example_Code/daxpy
+PROJECT_BUILD_PATH=$PROJECT_PATH/build
 ################
 
 ### DEPENDENT SETTINGS
@@ -26,11 +27,15 @@ else
     exit 1
 fi
 
+# create code copy 
+cp -r $PROJECT_PATH $HOME_DIR/code_copy
+
+
 # clean environment
 rm -rf logs
 mkdir logs
-rm -f measurements.txt
-echo "suggestion_id;time;exit_code;" >> measurements.txt
+rm -f measurements.csv
+echo "suggestion_id;time;exit_code;" >> measurements.csv
 
 # get baseline measurement
 # prepare output
@@ -38,11 +43,18 @@ echo "suggestion_id;time;exit_code;" >> measurements.txt
     # clean environment
     cd $PROJECT_PATH
     make clean 
+    rm -rf $PROJECT_BUILD_PATH
     # compile program
-    make -f Makefile.gnu.serial 1>> $HOME_DIR/logs/baseline/log.txt 2>> $HOME_DIR/logs/baseline/log.txt
+    mkdir $PROJECT_BUILD_PATH
+    cd $PROJECT_BUILD_PATH
+    cmake ..
+    make -j15 1>> $HOME_DIR/logs/baseline/log.txt 2>> $HOME_DIR/logs/baseline/log.txt
     # execute program
-    COMMAND="./miniFE.x --nx 40 --ny 40 --nz 40 verify_solution=1"
-    /usr/bin/time --format="baseline;%e;%x;" --append --output=$HOME_DIR/measurements.txt $COMMAND 1>> $HOME_DIR/logs/baseline/stdout.txt 2>> $HOME_DIR/logs/baseline/stderr.txt
+    COMMAND="./prog"
+    /usr/bin/time --format="baseline;%e;%x;" --append --output=$HOME_DIR/measurements.csv $COMMAND 1>> $HOME_DIR/logs/baseline/stdout.txt 2>> $HOME_DIR/logs/baseline/stderr.txt
+
+    # restore original state
+    cp -r $HOME_DIR/code_copy $PROJECT_PATH
 
 
 
@@ -56,20 +68,29 @@ for d in * ; do
     # clean environment
     cd $PROJECT_PATH
     make clean 
+    rm -rf $PROJECT_BUILD_PATH
 
     # apply suggestion
     cd $PROJECT_DP_FOLDER
     discopop_patch_applicator -a ${d} -v >> $HOME_DIR/logs/$d/log.txt
 
     # compile program
-    cd $PROJECT_PATH
-    make -f Makefile.discopop.openmp 1>> $HOME_DIR/logs/$d/log.txt 2>> $HOME_DIR/logs/$d/log.txt
+    mkdir $PROJECT_BUILD_PATH
+    cd $PROJECT_BUILD_PATH
+    cmake .. -DCMAKE_C_FLAGS="-fopenmp" -DCMAKE_CXX_FLAGS="-fopenmp"
+    make -j 15 1>> $HOME_DIR/logs/$d/log.txt 2>> $HOME_DIR/logs/$d/log.txt
 
     # execute program
-    /usr/bin/time --format="${d};%e;%x;" --append --output=$HOME_DIR/measurements.txt $COMMAND 1>> $HOME_DIR/logs/$d/stdout.txt 2>>$HOME_DIR/logs/$d/stderr.txt
+    /usr/bin/time --format="${d};%e;%x;" --append --output=$HOME_DIR/measurements.csv $COMMAND 1>> $HOME_DIR/logs/$d/stdout.txt 2>>$HOME_DIR/logs/$d/stderr.txt
 
     # rollback suggestions
     cd $PROJECT_DP_FOLDER
     discopop_patch_applicator -C -v >> $HOME_DIR/logs/$d/log.txt
+
+    # restore original state
+    cp -r $HOME_DIR/code_copy $PROJECT_PATH
 done
+
+# delete code copy 
+# rm -r $HOME_DIR/code_copy
 

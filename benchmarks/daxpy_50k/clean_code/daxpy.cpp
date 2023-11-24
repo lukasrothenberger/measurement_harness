9f6b5ee56a8d3c7b3fc2d0800c0b5355620dd116
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-24 14:17:54 +0000

    fixed daxpy benchmark code

--- benchmarks/daxpy_50k/clean_code/daxpy.cpp
+++ benchmarks/daxpy_50k/clean_code/daxpy.cpp
@@ -6,12 +6,6 @@ void daxpy(int n, double a, double *x, double *y) {
         y[i] = a * x[i] + y[i];
 }
 
-void foo(){
-    for (int i = 0; i < 100; i ++){
-        int y = 42 * i;    
-    }
-}
-
 int main(int argc, const char* argv[]) {
     static int n = 50000; static double a = 2.0; //n = 100000000;
     double *x = (double *) malloc(n * sizeof(double));
@@ -20,7 +14,6 @@ int main(int argc, const char* argv[]) {
     for(int i = 0; i < n; ++i){
         x[i] = 1.0;
         y[i] = 2.0;
-        foo();
     }
     daxpy(n, a, x, y); // Invoke daxpy kernel
     // Check if all values are 4.0

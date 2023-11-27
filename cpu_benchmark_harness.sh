cc9cff24366f25cbd72350db0be0e6eae940863b
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2023-11-27 13:02:18 +0100

    added cpu_harness

---
+++ cpu_benchmark_harness.sh
@@ -0,0 +1,21 @@
+ HOME_DIR=$(pwd)
+ BENCHMARKS_DIR=$HOME_DIR/benchmarks
+
+# iterate over all benchmarks and clean the environment
+cd $BENCHMARKS_DIR
+for bm in * ; do
+    echo "Cleaning benchmark environment: ${bm}"
+    cd $bm
+    ./clean.sh
+    cd $BENCHMARKS_DIR
+done
+
+# iterate over all benchmarks and execute the included harness
+cd $BENCHMARKS_DIR
+for bm in * ; do
+    echo "Executing benchmark: ${bm}"
+    cd $bm
+    ./cpu_harness.sh
+    cd $BENCHMARKS_DIR
+done
+

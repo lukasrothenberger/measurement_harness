3e544f0dde6e582d927260df7eb665e7034d9672
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-24 16:03:23 +0100

    discopop_optimizer: replaced -o with -o1

--- benchmarks/CoHMM/build_and_measure.sh
+++ benchmarks/CoHMM/build_and_measure.sh
@@ -12,7 +12,7 @@ else
     ./cohmm 1
     cd .discopop
     discopop_explorer --enable-patterns doall,reduction
-    discopop_optimizer -v -o --doall-microbench-file $BASEDIR/../../configuration/doall_1.json --system-configuration $BASEDIR/../../configuration/cpu_only_system_configuration.json
+    discopop_optimizer -v -o1 --doall-microbench-file $BASEDIR/../../configuration/doall_1.json --system-configuration $BASEDIR/../../configuration/cpu_only_system_configuration.json
     discopop_patch_generator -a optimizer/patterns.json
     cd $BASEDIR
     mv code original_build

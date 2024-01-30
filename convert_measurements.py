e7209c66fd5e7a08f9c413208161e8c593fbbe87
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-30 15:41:37 +0100

    automatically create formatted measurement reports

--- convert_measurements.py
+++ convert_measurements.py
@@ -1,5 +1,6 @@
 
 # Import the modules 
+from pathlib import Path
 from typing import List
 import warnings
 import matplotlib.pyplot as plt 
@@ -7,16 +8,26 @@ from matplotlib.backends.backend_pdf import PdfPages
 import sys
 import os
   
-# get paths to measurement.csv
-file_paths = sys.argv[1:]
-current_idx = 1
-valid_file_paths: List[str] = []
-for file_path in file_paths:
-    if not os.path.exists(file_path):
-        warnings.warn("FileNotFound: " + file_path + " --> Skipping")
-        continue
-    valid_file_paths.append(file_path)
 
+valid_file_paths: List[str] = []
+
+# get paths to measurement.csv from command line arguments, if supplied
+if len(sys.argv) > 1:
+    file_paths = sys.argv[1:]
+    for file_path in file_paths:
+        if not os.path.exists(file_path):
+            warnings.warn("FileNotFound: " + file_path + " --> Skipping")
+            continue
+        valid_file_paths.append(file_path)
+
+else:
+    # get paths to measurements from searching
+    for file_path in Path(os.getcwd()).rglob('measurements.csv'):
+        print(file_path.name , "->", file_path)
+        if not os.path.exists(file_path):
+            warnings.warn("FileNotFound: " + file_path + " --> Skipping")
+            continue
+        valid_file_paths.append(file_path)
 
 # configure plot
 cols = int(1)
@@ -24,7 +35,8 @@ rows = int((len(valid_file_paths) + (len(valid_file_paths) % 2)) / cols)
 fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(10*cols, 5*rows))
 fig.tight_layout(pad=8)
 
-for file_path in file_paths:
+current_idx = 1
+for file_path in valid_file_paths:
     print("Parsing:", file_path)
     # create subplot for each measurement file
 

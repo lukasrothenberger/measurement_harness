0b41d26cb38de082c1f336b04f9a7ec104044bf3
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-27 15:36:33 +0100

    minor

--- benchmarks/NPB/CG/build_and_measure.sh
+++ benchmarks/NPB/CG/build_and_measure.sh
@@ -69,7 +69,7 @@ do
     then
         echo "--> Executable exists. Skipping."
     else
-        CC=clang++ CXX=clang++ CC_FLAGS=-fopenmp make cg CLASS=B
+        CC=clang++ CXX=clang++ CC_FLAGS=-fopenmp make cg CLASS=A
     fi
 done
 

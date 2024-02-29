0fb0306b34ff9da78c2ec1117ebc26b5563191b8
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-29 11:52:38 +0100

    prepare covariance

--- clean_code/polybench_seq/datamining/covariance/covariance.c
+++ clean_code/polybench_seq/datamining/covariance/covariance.c
@@ -22,7 +22,7 @@
 static
 void init_array (int m, int n,
 		 DATA_TYPE *float_n,
-		 DATA_TYPE POLYBENCH_2D(data,M,N,m,n))
+		 DATA_TYPE POLYBENCH_1D(data,M*N,m*n))
 {
   int i, j;
 
@@ -30,7 +30,7 @@ void init_array (int m, int n,
 
   for (i = 0; i < M; i++)
     for (j = 0; j < N; j++)
-      data[i][j] = ((DATA_TYPE) i*j) / M;
+      data[i*m+j] = ((DATA_TYPE) i*j) / M;
 }
 
 
@@ -38,14 +38,14 @@ void init_array (int m, int n,
    Can be used also to check the correctness of the output. */
 static
 void print_array(int m,
-		 DATA_TYPE POLYBENCH_2D(symmat,M,M,m,m))
+		 DATA_TYPE POLYBENCH_1D(symmat,M*M,m*m))
 
 {
   int i, j;
 
   for (i = 0; i < m; i++)
     for (j = 0; j < m; j++) {
-      fprintf (stderr, DATA_PRINTF_MODIFIER, symmat[i][j]);
+      fprintf (stderr, DATA_PRINTF_MODIFIER, symmat[i*m+j]);
       if ((i * m + j) % 20 == 0) fprintf (stderr, "\n");
     }
   fprintf (stderr, "\n");
@@ -57,8 +57,8 @@ void print_array(int m,
 static
 void kernel_covariance(int m, int n,
 		       DATA_TYPE float_n,
-		       DATA_TYPE POLYBENCH_2D(data,M,N,m,n),
-		       DATA_TYPE POLYBENCH_2D(symmat,M,M,m,m),
+		       DATA_TYPE POLYBENCH_1D(data,M*N,m*n),
+		       DATA_TYPE POLYBENCH_1D(symmat,M*M,m*m),
 		       DATA_TYPE POLYBENCH_1D(mean,M,m))
 {
   int i, j, j1, j2;
@@ -68,23 +68,23 @@ void kernel_covariance(int m, int n,
     {
       mean[j] = 0.0;
       for (i = 0; i < _PB_N; i++)
-        mean[j] += data[i][j];
+        mean[j] += data[i*m+j];
       mean[j] /= float_n;
     }
 
   /* Center the column vectors. */
   for (i = 0; i < _PB_N; i++)
     for (j = 0; j < _PB_M; j++)
-      data[i][j] -= mean[j];
+      data[i*m+j] -= mean[j];
 
   /* Calculate the m * m covariance matrix. */
   for (j1 = 0; j1 < _PB_M; j1++)
     for (j2 = j1; j2 < _PB_M; j2++)
       {
-        symmat[j1][j2] = 0.0;
+        symmat[j1*m+j2] = 0.0;
         for (i = 0; i < _PB_N; i++)
-	  symmat[j1][j2] += data[i][j1] * data[i][j2];
-        symmat[j2][j1] = symmat[j1][j2];
+	  symmat[j1*m+j2] += data[i*m+j1] * data[i*m+j2];
+        symmat[j2*m+j1] = symmat[j1*m+j2];
       }
 
 }
@@ -98,8 +98,8 @@ int main(int argc, char** argv)
 
   /* Variable declaration/allocation. */
   DATA_TYPE float_n;
-  POLYBENCH_2D_ARRAY_DECL(data,DATA_TYPE,M,N,m,n);
-  POLYBENCH_2D_ARRAY_DECL(symmat,DATA_TYPE,M,M,m,m);
+  POLYBENCH_1D_ARRAY_DECL(data,DATA_TYPE,M*N,m*n);
+  POLYBENCH_1D_ARRAY_DECL(symmat,DATA_TYPE,M*M,m*m);
   POLYBENCH_1D_ARRAY_DECL(mean,DATA_TYPE,M,m);
 
 

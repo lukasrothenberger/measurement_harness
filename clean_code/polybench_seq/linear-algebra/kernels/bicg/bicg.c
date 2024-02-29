204d6a2e71d61e0bc39ee927519eab0cd8555ec1
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-29 09:50:39 +0100

    modified polybench: bicg

--- clean_code/polybench_seq/linear-algebra/kernels/bicg/bicg.c
+++ clean_code/polybench_seq/linear-algebra/kernels/bicg/bicg.c
@@ -21,7 +21,7 @@
 /* Array initialization. */
 static
 void init_array (int nx, int ny,
-		 DATA_TYPE POLYBENCH_2D(A,NX,NY,nx,ny),
+		 DATA_TYPE POLYBENCH_1D(A,NX*NY,nx*ny),
 		 DATA_TYPE POLYBENCH_1D(r,NX,nx),
 		 DATA_TYPE POLYBENCH_1D(p,NY,ny))
 {
@@ -32,7 +32,7 @@ void init_array (int nx, int ny,
   for (i = 0; i < nx; i++) {
     r[i] = i * M_PI;
     for (j = 0; j < ny; j++)
-      A[i][j] = ((DATA_TYPE) i*(j+1))/nx;
+      A[i*nx+j] = ((DATA_TYPE) i*(j+1))/nx;
   }
 }
 
@@ -63,7 +63,7 @@ void print_array(int nx, int ny,
    including the call and return. */
 static
 void kernel_bicg(int nx, int ny,
-		 DATA_TYPE POLYBENCH_2D(A,NX,NY,nx,ny),
+		 DATA_TYPE POLYBENCH_1D(A,NX*NY,nx*ny),
 		 DATA_TYPE POLYBENCH_1D(s,NY,ny),
 		 DATA_TYPE POLYBENCH_1D(q,NX,nx),
 		 DATA_TYPE POLYBENCH_1D(p,NY,ny),
@@ -78,8 +78,8 @@ void kernel_bicg(int nx, int ny,
       q[i] = 0;
       for (j = 0; j < _PB_NY; j++)
 	{
-	  s[j] = s[j] + r[i] * A[i][j];
-	  q[i] = q[i] + A[i][j] * p[j];
+	  s[j] = s[j] + r[i] * A[i*nx+j];
+	  q[i] = q[i] + A[i*nx+j] * p[j];
 	}
     }
 
@@ -93,7 +93,7 @@ int main(int argc, char** argv)
   int ny = NY;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NX, NY, nx, ny);
+  POLYBENCH_1D_ARRAY_DECL(A, DATA_TYPE, NX*NY, nx*ny);
   POLYBENCH_1D_ARRAY_DECL(s, DATA_TYPE, NY, ny);
   POLYBENCH_1D_ARRAY_DECL(q, DATA_TYPE, NX, nx);
   POLYBENCH_1D_ARRAY_DECL(p, DATA_TYPE, NY, ny);

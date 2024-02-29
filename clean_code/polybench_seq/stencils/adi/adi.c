ed5c148d3b2cb446b2b6b978949cd582a1f73f8d
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-29 12:01:24 +0100

    prepare adi

--- clean_code/polybench_seq/stencils/adi/adi.c
+++ clean_code/polybench_seq/stencils/adi/adi.c
@@ -21,18 +21,18 @@
 /* Array initialization. */
 static
 void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(X,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+		 DATA_TYPE POLYBENCH_1D(X,N*N,n*n),
+		 DATA_TYPE POLYBENCH_1D(A,N*N,n*n),
+		 DATA_TYPE POLYBENCH_1D(B,N*N,n*n))
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
       {
-	X[i][j] = ((DATA_TYPE) i*(j+1) + 1) / n;
-	A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
-	B[i][j] = ((DATA_TYPE) i*(j+3) + 3) / n;
+	X[i*n+j] = ((DATA_TYPE) i*(j+1) + 1) / n;
+	A[i*n+j] = ((DATA_TYPE) i*(j+2) + 2) / n;
+	B[i*n+j] = ((DATA_TYPE) i*(j+3) + 3) / n;
       }
 }
 
@@ -41,14 +41,14 @@ void init_array (int n,
    Can be used also to check the correctness of the output. */
 static
 void print_array(int n,
-		 DATA_TYPE POLYBENCH_2D(X,N,N,n,n))
+		 DATA_TYPE POLYBENCH_1D(X,N*N,n*n))
 
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      fprintf(stderr, DATA_PRINTF_MODIFIER, X[i][j]);
+      fprintf(stderr, DATA_PRINTF_MODIFIER, X[i*n+j]);
       if ((i * N + j) % 20 == 0) fprintf(stderr, "\n");
     }
   fprintf(stderr, "\n");
@@ -60,9 +60,9 @@ void print_array(int n,
 static
 void kernel_adi(int tsteps,
 		int n,
-		DATA_TYPE POLYBENCH_2D(X,N,N,n,n),
-		DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+		DATA_TYPE POLYBENCH_1D(X,N*N,n*n),
+		DATA_TYPE POLYBENCH_1D(A,N*N,n*n),
+		DATA_TYPE POLYBENCH_1D(B,N*N,n*n))
 {
   int t, i1, i2;
 
@@ -71,29 +71,29 @@ void kernel_adi(int tsteps,
       for (i1 = 0; i1 < _PB_N; i1++)
 	for (i2 = 1; i2 < _PB_N; i2++)
 	  {
-	    X[i1][i2] = X[i1][i2] - X[i1][i2-1] * A[i1][i2] / B[i1][i2-1];
-	    B[i1][i2] = B[i1][i2] - A[i1][i2] * A[i1][i2] / B[i1][i2-1];
+	    X[i1*n+i2] = X[i1*n+i2] - X[i1*n+i2-1] * A[i1*n+i2] / B[i1*n+i2-1];
+	    B[i1*n+i2] = B[i1*n+i2] - A[i1*n+i2] * A[i1*n+i2] / B[i1*n+i2-1];
 	  }
 
       for (i1 = 0; i1 < _PB_N; i1++)
-	X[i1][_PB_N-1] = X[i1][_PB_N-1] / B[i1][_PB_N-1];
+	X[i1*n+(_PB_N-1)] = X[i1*n+(_PB_N-1)] / B[i1*n+(_PB_N-1)];
 
       for (i1 = 0; i1 < _PB_N; i1++)
 	for (i2 = 0; i2 < _PB_N-2; i2++)
-	  X[i1][_PB_N-i2-2] = (X[i1][_PB_N-2-i2] - X[i1][_PB_N-2-i2-1] * A[i1][_PB_N-i2-3]) / B[i1][_PB_N-3-i2];
+	  X[i1*n+(_PB_N-i2-2)] = (X[i1*n+(_PB_N-2-i2)] - X[i1*n+(_PB_N-2-i2-1)] * A[i1*n+(_PB_N-i2-3)]) / B[i1*n+(_PB_N-3-i2)];
 
       for (i1 = 1; i1 < _PB_N; i1++)
 	for (i2 = 0; i2 < _PB_N; i2++) {
-	  X[i1][i2] = X[i1][i2] - X[i1-1][i2] * A[i1][i2] / B[i1-1][i2];
-	  B[i1][i2] = B[i1][i2] - A[i1][i2] * A[i1][i2] / B[i1-1][i2];
+	  X[i1*n+i2] = X[i1*n+i2] - X[(i1-1)*n+i2] * A[i1*n+i2] / B[(i1-1)*n+i2];
+	  B[i1*n+i2] = B[i1*n+i2] - A[i1*n+i2] * A[i1*n+i2] / B[(i1-1)*n+i2];
 	}
 
       for (i2 = 0; i2 < _PB_N; i2++)
-	X[_PB_N-1][i2] = X[_PB_N-1][i2] / B[_PB_N-1][i2];
+	X[(_PB_N-1)*n+i2] = X[(_PB_N-1)*n+i2] / B[(_PB_N-1)*n+i2];
 
       for (i1 = 0; i1 < _PB_N-2; i1++)
 	for (i2 = 0; i2 < _PB_N; i2++)
-	  X[_PB_N-2-i1][i2] = (X[_PB_N-2-i1][i2] - X[_PB_N-i1-3][i2] * A[_PB_N-3-i1][i2]) / B[_PB_N-2-i1][i2];
+	  X[(_PB_N-2-i1)*n+i2] = (X[(_PB_N-2-i1)*n+i2] - X[(_PB_N-i1-3)*n+i2] * A[(_PB_N-3-i1)*n+i2]) / B[(_PB_N-2-i1)*n+i2];
     }
 
 }
@@ -106,9 +106,9 @@ int main(int argc, char** argv)
   int tsteps = TSTEPS;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(X, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
-  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
+  POLYBENCH_1D_ARRAY_DECL(X, DATA_TYPE, N*N, n*n);
+  POLYBENCH_1D_ARRAY_DECL(A, DATA_TYPE, N*N, n*n);
+  POLYBENCH_1D_ARRAY_DECL(B, DATA_TYPE, N*N, n*n);
 
 
   /* Initialize array(s). */

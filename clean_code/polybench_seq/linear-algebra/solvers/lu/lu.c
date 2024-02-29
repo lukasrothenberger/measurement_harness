9f6e0565f5a522a76ef4a1df561c2a4fd049ff42
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-02-29 11:43:18 +0100

    prepare lu

--- clean_code/polybench_seq/linear-algebra/solvers/lu/lu.c
+++ clean_code/polybench_seq/linear-algebra/solvers/lu/lu.c
@@ -21,13 +21,13 @@
 /* Array initialization. */
 static
 void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+		 DATA_TYPE POLYBENCH_1D(A,N*N,n*n))
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
-      A[i][j] = ((DATA_TYPE) (i+1)*(j+1)) / n;
+      A[i*nj] = ((DATA_TYPE) (i+1)*(j+1)) / n;
 }
 
 
@@ -35,14 +35,14 @@ void init_array (int n,
    Can be used also to check the correctness of the output. */
 static
 void print_array(int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+		 DATA_TYPE POLYBENCH_1D(A,N*N,n*n))
 
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      fprintf (stderr, DATA_PRINTF_MODIFIER, A[i][j]);
+      fprintf (stderr, DATA_PRINTF_MODIFIER, A[i*n+j]);
       if ((i * n + j) % 20 == 0) fprintf (stderr, "\n");
     }
   fprintf (stderr, "\n");
@@ -53,17 +53,17 @@ void print_array(int n,
    including the call and return. */
 static
 void kernel_lu(int n,
-	       DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+	       DATA_TYPE POLYBENCH_1D(A,N*N,n*n))
 {
   int i, j, k;
 
   for (k = 0; k < _PB_N; k++)
     {
       for (j = k + 1; j < _PB_N; j++)
-	A[k][j] = A[k][j] / A[k][k];
+	A[k*n+j] = A[k*n+j] / A[k*n+k];
       for(i = k + 1; i < _PB_N; i++)
 	for (j = k + 1; j < _PB_N; j++)
-	  A[i][j] = A[i][j] - A[i][k] * A[k][j];
+	  A[i*n+j] = A[i*n+j] - A[i*n+k] * A[k*n+j];
     }
 
 }
@@ -75,7 +75,7 @@ int main(int argc, char** argv)
   int n = N;
 
   /* Variable declaration/allocation. */
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
+  POLYBENCH_1D_ARRAY_DECL(A, DATA_TYPE, N*N, n*n);
 
 
   /* Initialize array(s). */

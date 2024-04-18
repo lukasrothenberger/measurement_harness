ffa5fc8707ecb0f2ab9361838f9287d4e0ef5cf9
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-04-18 09:43:01 +0200

    minor

--- benchmarks/NPB/EP/build_and_measure.sh
+++ benchmarks/NPB/EP/build_and_measure.sh
@@ -25,14 +25,14 @@ else
 #    make clean
     # discopop pattern detection
     
-    DOT_DISCOPOP=$BASEDIR/code/bin/.discopop CXX=discopop_cxx CC=discopop_cc make ep
+    DOT_DISCOPOP=$BASEDIR/code/bin/.discopop CXX=discopop_cxx CC=discopop_cc make ep CLASS=S
     
     cd bin
     ./ep.S
 
     cd .discopop
     discopop_explorer --enable-patterns doall,reduction
-    discopop_optimizer -v -s --doall-microbench-file $BASEDIR/../../../configuration/doall_1.json --system-configuration $BASEDIR/../../../configuration/cpu_only_system_configuration.json
+    discopop_optimizer -v -s -o1 --doall-microbench-file $BASEDIR/../../../configuration/doall_1.json --system-configuration $BASEDIR/../../../configuration/cpu_only_system_configuration.json
     discopop_patch_generator -a optimizer/patterns.json ${DP_PATCH_GENERATOR_FLAGS}
     cd $BASEDIR
     mv code original_build

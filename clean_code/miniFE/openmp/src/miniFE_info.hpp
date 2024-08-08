75e65b9cdf071cd7eda47585892a2a93647cf329
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-08 10:43:44 +0200

    add autotuner: miniFE

---
+++ clean_code/miniFE/openmp/src/miniFE_info.hpp
@@ -0,0 +1,13 @@
+#ifndef miniFE_info_hpp
+#define miniFE_info_hpp
+
+#define MINIFE_HOSTNAME "lukasT14s"
+#define MINIFE_KERNEL_NAME "'Linux'"
+#define MINIFE_KERNEL_RELEASE "'5.15.0-112-generic'"
+#define MINIFE_PROCESSOR "'x86_64'"
+
+#define MINIFE_CXX "'/usr/bin/g++'"
+#define MINIFE_CXX_VERSION "'g++ (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0'"
+#define MINIFE_CXXFLAGS "'-O3 -fopenmp'"
+
+#endif
---
+++ clean_code/miniFE/ref/DP_COMPILE.sh
@@ -0,0 +1,4 @@
+cd src
+make clean
+make -f Makefile.gnu.openmp
+cd ..

91866569ccc43e7e64ae4bc17f7432c0d99138d9
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-08-09 13:55:01 +0200

    miniFE increased data size

--- clean_code/miniFE/openmp/DP_EXECUTE.sh
+++ clean_code/miniFE/openmp/DP_EXECUTE.sh
@@ -1,5 +1,5 @@
 cd src
-./miniFE.x --nx 100 --ny 100 --nz 100 --verify_solution=1 > dp_execute_out.txt
+./miniFE.x --nx 125 --ny 125 --nz 125 --verify_solution=1 > dp_execute_out.txt
 grep "solution matches analytic solution" dp_execute_out.txt
 retval=$?
 cd ..
